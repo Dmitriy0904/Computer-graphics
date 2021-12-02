d15af4b1ab85b7812a7a72b498fcce77ff3cb10a
Dmitriy <dmitriygridin0904@gmail.com>
2021-12-03 02:00:27 +0300

    Updated task-1 model. Added project lombock. Getters, setters and constructor have replaced by using lombovks annotations

--- task-1/src/main/java/model/Square.java
+++ task-1/src/main/java/model/Square.java
@@ -1,26 +1,15 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.awt.geom.Line2D;
-import java.util.ArrayList;
 import java.util.List;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Square {
     private List<Line2D> lines;
-
-    public Square() {
-        this.lines = new ArrayList<>();
-    }
-
-    public Square(List<Line2D> lines) {
-        this.lines = lines;
-    }
-
-    public List<Line2D> getLines() {
-        return lines;
-    }
-
-    public void setLines(List<Line2D> lines) {
-        this.lines = lines;
-    }
-
 }
