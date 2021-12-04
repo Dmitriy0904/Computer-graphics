a2ac0b9859527c83016301ddea3df1422da2a0f8
Dmitriy <dmitriygridin0904@gmail.com>
2021-12-05 01:24:39 +0300

    Removed lombock annotations. Added standart constructors, getters and setters

--- task-1/src/main/java/model/Square.java
+++ task-1/src/main/java/model/Square.java
@@ -1,15 +1,23 @@
 package model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.awt.geom.Line2D;
 import java.util.List;
 
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
+
 public class Square {
     private List<Line2D> lines;
+
+    public Square() { }
+
+    public Square(List<Line2D> lines) {
+        this.lines = lines;
+    }
+
+    public List<Line2D> getLines() {
+        return lines;
+    }
+
+    public void setLines(List<Line2D> lines) {
+        this.lines = lines;
+    }
 }
