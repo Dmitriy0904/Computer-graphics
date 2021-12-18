fc160eecfc4b24d9cb5df8ea68751cdd826d6867
Dmitriy <dmitriygridin0904@gmail.com>
2021-12-18 17:46:53 +0300

    Task-2. Updated form class

--- task-2/src/main/java/form/Form.java
+++ task-2/src/main/java/form/Form.java
@@ -8,6 +8,8 @@ import point.Point;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.Point2D;
+import java.util.List;
 
 public class Form extends JFrame {
     private final JPanel panel = new JPanel();
@@ -21,22 +23,30 @@ public class Form extends JFrame {
     public void paint(Graphics graphics) {
         super.paint(graphics);
 
+        //1
         Cube cube = CubeCreator.createCube();
+//        Projection first = new Projection(CubeUtil.project(cube));
+//        first.paint(graphics);
 
+
+        //2
+//        Point shiftPoint =  new Point(340, 280, 0);
+//        CubeUtil.shift(cube, shiftPoint);
+//        Projection second = new Projection(CubeUtil.project(cube));
+//        second.paint(graphics);
+
+
+        //3
         Cube rotatedCube = CubeUtil.rotateAboutArbitraryLine(
                 cube,
                 new Point(0, 0, 0),
-                new Point(25, 30, 35),
+                new Point(25, 25, 25),
                 Math.toRadians(45)
         );
 
         Point shiftPoint =  new Point(340, 280, 0);
         CubeUtil.shift(rotatedCube, shiftPoint);
-
-        Projection rotatedCubeProjection = new Projection(CubeUtil.project(cube));
-        rotatedCubeProjection.paint(graphics);
-
-        Projection ShiftedRotatedCubeProjection = new Projection(CubeUtil.project(rotatedCube));
-        ShiftedRotatedCubeProjection.paint(graphics);
+        Projection third = new Projection(CubeUtil.project(rotatedCube));
+        third.paint(graphics);
     }
 }
