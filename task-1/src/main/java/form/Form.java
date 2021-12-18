357b512709312a01afcca5de5f0a61faf7a53237
Dmitriy <dmitriygridin0904@gmail.com>
2021-12-18 17:46:03 +0300

    Task-1. Code updates. Removed controller class

--- task-1/src/main/java/form/Form.java
+++ task-1/src/main/java/form/Form.java
@@ -1,19 +1,14 @@
 package form;
 
-import controller.Controller;
 import model.Square;
 import util.SquareCreator;
 import util.SquareUtil;
 import javax.swing.*;
 import java.awt.*;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.awt.geom.Point2D;
 
 
 public class Form extends JFrame {
-    private final Controller controller = new Controller();
-    private BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
     private JPanel panel = new JPanel();
 
     public Form() {
@@ -25,25 +20,13 @@ public class Form extends JFrame {
     public void paint(Graphics graphics) {
 
         Square square = SquareCreator.newSquare();
+        SquareUtil.paint(square, graphics);
 
-        while(true){
-            try{
-                SquareUtil.paint(square, graphics);
+        SquareUtil.parallelMove(square, 100, 100);
+        SquareUtil.paint(square, graphics);
 
-                System.out.println("1-Parallel move, 2-Rotate, 0-Exit");
-                int choose = Integer.parseInt(bf.readLine());
-
-                switch (choose){
-                    case 1 -> controller.parallelMove(square);
-                    case 2 -> controller.rotate(square);
-                    case 0 -> System.exit(1);
-                    default -> System.out.println("Such choose does not supported");
-                }
-
-            } catch (IOException exception){
-                throw new RuntimeException(exception.getMessage());
-            }
-        }
+        SquareUtil.rotate(square, new Point2D.Double(570, 300), 60);
+        SquareUtil.paint(square, graphics);
 
     }
 }
